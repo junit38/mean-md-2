52751ec32afb1b157a89c3e953819905b471d367
Junit38 <Junit38@gmail.com>
2019-05-31 18:23:43 +0200

    Add angularjs material design in users clients settings views

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -5,11 +5,27 @@
     .module('users')
     .controller('SettingsController', SettingsController);
 
-  SettingsController.$inject = ['$scope', 'Authentication'];
+  SettingsController.$inject = ['$scope', '$rootScope', '$location', 'Authentication'];
 
-  function SettingsController($scope, Authentication) {
+  function SettingsController($scope, $rootScope, $location, Authentication) {
     var vm = this;
 
     vm.user = Authentication.user;
+    vm.currentNavItem = getCurrentLocation();
+
+    $rootScope.$on('$locationChangeStart', function () {
+      vm.currentNavItem = getCurrentLocation();
+    });
+
+    function getCurrentLocation() {
+      if ($location.path() == '/settings/picture')
+        return ('change_profile_picture');
+      else if ($location.path() == '/settings/password')
+        return ('change_password');
+      else if ($location.path() == '/settings/accounts')
+        return ('manage_social_accounts');
+      else
+        return ('edit_profile');
+    }
   }
 }());
